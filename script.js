ab09fa656c35b9686008f84508dad46cf5791bce
Babakbigdeli <babakbigdeli@gmail.com>
2020-11-29 16:19:17 +0000

    adding more ids to html and current date and time and rendering elements to js

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
 let apiKey = "eb41e266ed6ca53d9946184d90b8d779";
-let searchBtn = $(".search-button");
-let searchInput = $(".form-control");
+let searchBtn = $("#search-button");
+let searchInput = $("#city-input");
 
-// Search container and current date variables
-let cityNameEl = $("#cityName");
-let currentDateEl = $(".currentDate");
+// Search container and current date variab
 let weatherIconEl = $("#current-pic");
 let searchHistoryEl = $("#history");
+let cityNameEl = $("#city-name");
+let currentDateEl = $(".currentDate");
+let clearEl = $("#clear-history");
 let tempEl = $("#temperature");
 let humidityEl = $("#humidity");
 let windSpeedEl = $("#wind-speed");
 let uvIndexEl = $("#UV-index");
 let cardRow = $(".card-row");
+
+
 
 // Create a current date variable
 var today = new Date();
@@ -20,6 +23,48 @@ let mm = String(today.getMonth() + 1).padStart(2, '0');
 let yyyy = today.getFullYear();
 var today = dd + '/' + mm + '/' + yyyy;
 
+if (JSON.parse(localStorage.getItem("searchHistory")) === null) {
+    console.log("searchHistory not found")
+}else{
+    console.log("searchHistory loaded into searchHistoryArr");
+    renderSearchHistory();
+}
 
+
+searchBtn.on("click", function(e) {
+    e.preventDefault();
+    if (searchInput.val() === "") {
+        alert("You must enter a city");
+        return;
+    }
+    console.log("clicked button")
+    getWeather(searchInput.val());
+});
+
+$(document).on("click", ".historyEntry", function() {
+    console.log("clicked history item")
+    let thisElement = $(this);
+    getWeather(thisElement.text());
+})
+
+function renderSearchHistory(cityName) {
+    searchHistoryEl.empty();
+    let searchHistoryArr = JSON.parse(localStorage.getItem("searchHistory"));
+    for (let i = 0; i < searchHistoryArr.length; i++) {
+        let newListItem = $("<li>").attr("class", "historyEntry");
+        newListItem.text(searchHistoryArr[i]);
+        searchHistoryEl.prepend(newListItem);
+    }
+}
+
+function renderWeatherData(cityName, cityTemp, cityHumidity, cityWindSpeed, cityWeatherIcon, uvVal) {
+    cityNameEl.text(cityName)
+    currentDateEl.text(`(${today})`)
+    tempEl.text(`Temperature: ${cityTemp} °F`);
+    humidityEl.text(`Humidity: ${cityHumidity}%`);
+    windSpeedEl.text(`Wind Speed: ${cityWindSpeed} MPH`);
+    uvIndexEl.text(`UV Index: ${uvVal}`);
+    weatherIconEl.attr("src", cityWeatherIcon);
+}
 
 
